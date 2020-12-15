6970911325d7e491de2586b6e3f76805dc979b75
Alan <alan.lorenzetti@gmail.com>
2020-12-14 23:02:44 -0300

    scripts renamed

--- scripts/scaffold.R
+++ scripts/scaffold.R
@@ -31,9 +31,8 @@ if(!dir.exists("results")){dir.create("results")}
 
 # sourcing ####
 # loading libs
-source("scripts/loadingLibs.R")
-if(reportFlag == "yes"){source("scripts/qualityControl.R")}
-source("scripts/functionalCategorization.R")
-source("scripts/deAnalysis.R")
-source("scripts/results.R")
-
+source("scripts/1_loadingLibs.R")
+if(reportFlag == "yes"){source("scripts/2_qualityControl.R")}
+source("scripts/3_functionalCategorization.R")
+source("scripts/4_deAnalysis.R")
+source("scripts/5_results.R")
